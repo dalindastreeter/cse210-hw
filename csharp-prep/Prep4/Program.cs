b22b16a537645149c986f0289d43d56aef456e85
dalindastreeter <str22002@byui.edu>
2024-09-24 00:15:18 -0500

    average

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -25,10 +25,15 @@ class Program
         int sum = 0;
          
         foreach (int number in numbers)
-            {
-             sum += number;
-            }
+        {
+            sum += number;
+        }
+            
         Console.WriteLine("The sum is: " + sum);
+        float average=sum/numbers.Count; 
+         
+        Console.WriteLine("The average of the numbers is: " + average);
+            
     }
 }
  
