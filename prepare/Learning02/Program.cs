d16d48b989bc28c813ca946fd25ff43ea4576517
dalindastreeter <str22002@byui.edu>
2024-09-27 15:25:49 -0500

    steps 1-5

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        
+        Job job1 = new Job();
+        job1._company =  "Apple";
+        job1._title = "Sofware Developer";
+        job1._startYear = 2012;
+        job1._endYear = 2014;
+        
+        job1.Display();
+
+         Job job2= new Job();
+        job2._company =  "Facebook";
+        job2._title = "Staff Engineer";
+        job2._startYear = 2014;
+        job2._endYear = 2018;
+        
+        job2.Display();
+        
+        
+
     }
 }
