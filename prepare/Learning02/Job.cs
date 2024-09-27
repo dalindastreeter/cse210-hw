2411a5a8f86321b628c9d1df823390cfc10fc102
dalindastreeter <str22002@byui.edu>
2024-09-27 18:34:21 -0500

    created the class

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+public class Job 
+
+{   
+    public string _company;
+    public string _title;
+    public int _startYear;
+    public int _endYear;
+    public Job ()
+    {}
+
+    public  void Display()
+    {
+        Console.WriteLine($"{_title} ({_company}) {_startYear}-{_endYear}");
+    }
+}
