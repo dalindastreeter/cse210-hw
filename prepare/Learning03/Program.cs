88bdeb7d8229017d1a3645f3164ff06e464dd267
dalindastreeter <str22002@byui.edu>
2024-10-06 19:26:06 -0500

    usisng the class fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction nopara = new Fraction();
+        Console.WriteLine(nopara.GetFractionString());
+        Console.WriteLine(nopara.GetDecimalValue());
+
+        Fraction whole = new Fraction(5);
+        Console.WriteLine(whole.GetFractionString());
+        Console.WriteLine(whole.GetDecimalValue());
+
+        Fraction ambos= new Fraction(3,4);
+        Console.WriteLine(ambos.GetFractionString());
+        Console.WriteLine(ambos.GetDecimalValue());
+
+        Fraction ambosb= new Fraction(1,3);
+        Console.WriteLine(ambosb.GetFractionString());
+        Console.WriteLine(ambosb.GetDecimalValue());
+
+
+
     }
 }
